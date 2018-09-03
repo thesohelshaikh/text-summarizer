490ea1e907d7351e8c6ad4e6109f8a554776ade4
Sohel Shaikh <sohelshaikh97@gmail.com>
2018-09-04 00:07:06 +0530

    Added validation

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect
+
+# For flashing a  message
+from flask import flash
 
 # For rendering html templates
 from flask import render_template
@@ -12,11 +15,14 @@ from forms import LinkForm
 # intatiates flask app, configures the application
 app = Flask(__name__) 
 
-app.config['SECRET_KEY'] = 'xyz'
+app.config['SECRET_KEY'] = 'This is a secret'
 
-@app.route("/")
+@app.route("/",  methods=['GET','POST'])
 def main():
     form = LinkForm()
+    if form.validate_on_submit():
+        flash(f'Correct input!', 'success')
+        return redirect(url_for('main'))
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
