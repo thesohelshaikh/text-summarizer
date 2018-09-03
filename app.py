1fc05d87ebd55578cb67c0dd672e5d50c3c31886
Sohel Shaikh <sohelshaikh97@gmail.com>
2018-09-03 20:50:29 +0530

    Updated layout

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask
 # For rendering html templates
 from flask import render_template
 
+# For linking files, generates a URL
+from flask import url_for
+
 # intatiates flask app, configures the application
 app = Flask(__name__) 
 
 @app.route("/")
-def hello():
+def main():
     return render_template('index.html')
 
 if __name__ == '__main__':
